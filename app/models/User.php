10f54cd96b318808d72ace6210de39a6a09a1f6d
asen-krasimirov <personalgrowth57@gmail.com>
2025-06-06 14:15:16 +0300

    Fix auth

--- app/models/User.php
+++ app/models/User.php
@@ -4,16 +4,20 @@ require_once __DIR__ . '/../../config/database.php';
 class User {
     public static function findByEmail($email) {
         global $conn;
+        
         $stmt = $conn->prepare("SELECT * FROM users WHERE email = ?");
         $stmt->bind_param("s", $email);
         $stmt->execute();
+        
         return $stmt->get_result()->fetch_assoc();
     }
 
     public static function create($username, $email, $passwordHash) {
         global $conn;
+        
         $stmt = $conn->prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
         $stmt->bind_param("sss", $username, $email, $passwordHash);
+        
         return $stmt->execute();
     }
 }
